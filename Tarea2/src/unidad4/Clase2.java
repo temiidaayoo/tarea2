309450f397e15af0d269f59a1e24bd41b6d507fc
micekholu <micekholu@SICE-9635144706>
2025-03-31 19:22:47 +0200

    Creamos el Proyecto TArea 2 y hemos modificado y la clase impime 7 mss

---
+++ Tarea2/src/unidad4/Clase2.java
@@ -0,0 +1,8 @@
+package unidad4;
+
+//Clase Utils.java
+public class Clase2 {
+ public static void imprimirMensaje() {
+     System.out.println("Barcelona primero en liga");
+ }
+}
---
+++ Tarea2/src/unidad4/MenuPrincipal.java
@@ -0,0 +1,24 @@
+package unidad4;
+
+public class MenuPrincipal {
+    public static void main(String[] args) {
+        System.out.println("Hola Dario");
+        
+        
+        System.out.println("Hola 1");
+        
+        System.out.println("Hola 2");
+
+        System.out.println("Hola 3");
+
+        System.out.println("Hola 4");
+
+        System.out.println("Hola 5");
+
+        
+        System.out.println("Hola 6");
+
+
+        
+    }
+}
